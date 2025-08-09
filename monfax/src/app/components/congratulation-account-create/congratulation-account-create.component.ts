e623cf970819b4feabb6a11ceecd867fbf7bec75
ManichUser <151421854+ManichUser@users.noreply.github.com>
2025-08-09 08:13:44 +0100

    Transformation des pages en  standalone et correction routing

---
+++ monfax/src/app/components/congratulation-account-create/congratulation-account-create.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-congratulation-account-create',
+  templateUrl: './congratulation-account-create.component.html',
+  styleUrls: ['./congratulation-account-create.component.scss'],
+})
+export class CongratulationAccountCreateComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- monfax/src/app/tab1/tab1.page.ts
+++ monfax/src/app/tab1/tab1.page.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+
+import { CongratulationAccountCreateComponent } from '../components/congratulation-account-create/congratulation-account-create.component';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  standalone: false,
+  standalone: true,
+  imports: [
+    IonicModule,
+    CommonModule,
+    CongratulationAccountCreateComponent
+  ]
 })
-export class Tab1Page {
-
-  constructor() {}
-
-}
+export class Tab1Page {}
